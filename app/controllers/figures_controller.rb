3be303ae61653366c20921f9c2c7e73cc976ac8a
ian-ramos <ramos.ianc@gmail.com>
2017-09-20 09:10:37 -0400

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,49 @@
+class FiguresController < ApplicationController
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'figures/index'
+  end
+
+  get '/figures/new' do
+    erb :'figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title]) #passing in hash as argument
+      @figure.titles << @title #can't use .update(title.id) b/c figure doesn't have title id field?
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark]) #passing in hash as argument
+      @figure.landmarks << @landmark
+    end
+    redirect to '/figures'
+  end
+
+  get '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    if !params[:title][:name].empty?
+      @title = Title.create(params[:title])
+      @figure.titles << @title
+    end
+    if !params[:landmark][:name].empty?
+      @landmark = Landmark.create(params[:landmark])
+      @figure.landmarks << @landmark
+    end
+    redirect to "/figures/#{@figure.id}"
+  end
+
+end
